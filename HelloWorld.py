7b5054153ae524fa3a33eff288c407772f05da08
Hamza Ebrahim <35878787+HamzaEbrahim@users.noreply.github.com>
2018-05-29 23:15:40 +0200

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 14 16:50:41 2018
+
+@author: Hamza
+
+basic program asking for user info and printing hello world
+"""
+
+username = input("What is your name?\n")
+print(username)
+
+userage = input("How old are you?\n")
+print(userage)
+
+text = "Hello World!"
+print(text)
