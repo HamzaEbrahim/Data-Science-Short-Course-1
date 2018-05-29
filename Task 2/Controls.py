10e1853a8453b5cf435e6604034ce8758847ac72
Hamza Ebrahim <35878787+HamzaEbrahim@users.noreply.github.com>
2018-05-29 23:30:31 +0200

    simple program to demonstrate if/elif/else

--- Task 2/Controls.py
+++ Task 2/Controls.py
@@ -8,16 +8,16 @@ simple program to demonstrate if, elif and else statements. As well as for loop.
 
 """
 
-age = input("How old are you?\n")                                               # age variable, asks for user input. Recieves input as a string
-age = int(age)                                                                  # casts age variable from a string to an integer, to be used in if statement.
+age = input("How old are you?\n")                     # age variable, asks for user input. Recieves input as a string
+age = int(age)                                        # casts age variable from a string to an integer, to be used in if statement.
 
-if age >= 18:                                                                   # beginning of control structure with if, elif & else statements
-    print("You are old enough!")                                                # print statement outputs You are old enough if user is at least 18 years old
+if age >= 18:                                         # beginning of control structure with if, elif & else statements
+    print("You are old enough!")                      # print statement outputs You are old enough if user is at least 18 years old
 elif age > 16:
-    print("Almost there")                                                       # print statement outputs Almost there for users older than 16 but younger than 18
+    print("Almost there")                             # print statement outputs Almost there for users older than 16 but younger than 18
 else:
-    print("You are too young")                                                  # print statement outputs You are too young if user is under 16
+    print("You are too young")                        # print statement outputs You are too young if user is under 16
             
-for i in range(age):                                                            # for loop which prints every number from 0 to the user input for age variable
+for i in range(age):                                  # for loop which prints every number from 0 to the user input for age variable
     if i <= age:
         print(i)
